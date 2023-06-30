47fc26d1ba36c3cd8b7f60bce659deb575987d54
Hari Vishnu Parashar <harryskotch11@gmail.com>
2023-06-30 19:38:39 +0530

    fix: issues add global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,13 @@
 </script>
 
 <slot />
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.background);
+  }
+
+  :global(h1, h2, h3, h4, h5, h6, h7) {
+    color: theme(colors.text.headings);
+  }
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#FAFAF8',
+        card: {
+          background: '#F5F5F5'
+        },
+        text: {
+          headings: '#252525'
+        }
+      }
+    }
   },
   plugins: []
 };
